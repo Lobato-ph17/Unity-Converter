841d8de83b9a6fea5c756b7c85f6e9a6bbfcacaf
Pedro Lobato <lobato.ph17@gmail.com>
2025-12-03 19:55:29 -0300

    adicionado funções em javascript para conversão dos valores e função para dar clear no input

--- index.js
+++ index.js
@@ -4,8 +4,44 @@ const valueBotton = document.getElementById('input-value-botton')
 
 const convertBtn = document.getElementById('converter-btn')
 
+const unitFrom = document.getElementById('unit-select-from')
+
+const unitTo = document.getElementById('unit-select-to')
+
+
+const unitToMeter = {
+    m: 1,
+    km: 1000,
+    yd: 0.9144,
+    ft: 0.3048,
+    mi: 1609.34
+};
+
+// ---------------- EVENTOS DE CONVERSÃO ===================
+
+
 convertBtn.addEventListener('click', function(){
-    let result = Number(valueUpper.value) * 2
     
-    valueBotton.value = result
-})
+    const inputValue = Number(valueUpper.value)
+    const from = unitFrom.value
+    const to = unitTo.value
+
+    const valorEmMetros = inputValue * unitToMeter[from]
+    const resultadoFinal = valorEmMetros / unitToMeter[to]
+
+    valueBotton.value = resultadoFinal
+    })
+
+
+//  ------------------ Evento de clear ----------------------
+
+function clearOnFocus(input) {
+    input.addEventListener('focus', function () {
+        if (input.value !== '') {
+            input.value = '';
+        }
+    });
+}
+
+clearOnFocus(valueUpper);
+clearOnFocus(valueBotton);
